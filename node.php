5e32594b60da16cf57fd347de2b905d0222349ab
john Brandews <johnbrandews@gmail.com>
2024-07-01 17:31:32 +0300

    added another php file

---
+++ node.php
@@ -0,0 +1,14 @@
+<?php
+include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello <br>
+</body>
+</html>
