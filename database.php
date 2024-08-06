cc7024f72a1b2164c615b8616b65c0b7878c4864
john Brandews <johnbrandews@gmail.com>
2024-08-06 11:09:36 +0300

    added changes on the database example

--- database.php
+++ database.php
@@ -4,11 +4,13 @@ $db_user ="root";
 $db_pass = "";
 $db_name = "businessdb";
 $conn = "";
-$conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-if($conn){
-    echo "Connection established";
-}
-else{
-    echo "Connection failed";
-}
+
+try{
+    $conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+    }
+    catch(mysqli_sql_exception){
+        echo "could not connect to database";   
+    }
+      
+
 ?>
