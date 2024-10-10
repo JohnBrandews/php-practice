5ef2091db1a2226575763bd9e9504fbaacf43b60
john Brandews <johnbrandews@gmail.com>
2024-10-11 00:38:42 +0300

    added php file on object cloning

--- Car.php
+++ Car.php
@@ -1,8 +1,50 @@
-<?php
-namespace Vehicles;
+ <?php
+// namespace Vehicles;
 
+// class Car {
+//     public function start() {
+//         return "Vroom!";
+//     }
+// } -
 class Car {
-    public function start() {
-        return "Vroom!";
+    public $brand;
+    public $color;
+    public $mileage;
+
+    public function __construct($brand, $color, $mileage) {
+        $this->brand = $brand;
+        $this->color = $color;
+        $this->mileage = $mileage;
     }
+
+    public function drive($distance) {
+        $this->mileage += $distance;
+    }
+
+    public function __clone() {
+        // This method is called when the object is cloned
+        $this->mileage = 0; // Reset mileage for the new car
+    }
+}
+try{
+// Create an original car
+$originalCar = new Car("Toyota", "Blue", 50000);
+
+// Clone the car
+$clonedCar = clone $originalCar;
+
+// Modify the cloned car
+$clonedCar->color = "Red";
+$clonedCar->drive(200);
+
+// Drive the original car
+$originalCar->drive(100);
+
+// Output results
+echo "Original Car: " . $originalCar->brand . ", " . $originalCar->color . ", Mileage: " . $originalCar->mileage ."\n";
+echo "<br>";
+echo "Cloned Car: " . $clonedCar->brand . ", " . $clonedCar->color . ", Mileage: " . $clonedCar->mileage . "\n";
+}
+catch(Exception $e){
+echo "an error occurred: " . $e->getMessage();
 }
